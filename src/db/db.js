662f1e8783db596f6404d6ef1138a26d7f8a84a7
tux1765 <75902381+tux1765@users.noreply.github.com>
2025-07-17 17:33:04 -0500

    Default cloud db

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,20 @@
 import Dexie from 'dexie'
 import dexieCloud from 'dexie-cloud-addon'
 
-export const db = new Dexie('allergyDb', {addons: process.env.DEXIE_CLOUD ? [dexieCloud] : []})
+export const db = new Dexie('allergyDb', {addons: process.env.DEXIE_CLOUD_DISABLE ? [] : [dexieCloud] })
 
-if (process.env.DEXIE_CLOUD) {
+if (process.env.DEXIE_CLOUD_DISABLE) {
+	// TODO: figure out if this is really needed
+	db.version(1).stores({
+		foods: '++id, name, brand, notes, store, isSafe, dateAdded'
+	})
+} else {
 	console.debug('Initializing Cloud DB')
 	db.version(1).stores({
 		foods: '@id, name, brand, notes, store, isSafe, dateAdded'
 	})
 	db.cloud.configure({
-		databaseUrl: process.env.DEXIE_DB_URL,
+		databaseUrl: process.env.DEXIE_DB_URL ?? 'https://z0nawraem.dexie.cloud',
 		requireAuth: false // optional
-	});
-} else {
-	// TODO: figure out if this is really needed
-	db.version(1).stores({
-		foods: '++id, name, brand, notes, store, isSafe, dateAdded'
 	})
 }
