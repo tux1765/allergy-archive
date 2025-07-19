199c574f534532a631c6c7381efe7795cbcf9e7d
tux1765 <75902381+tux1765@users.noreply.github.com>
2025-07-19 10:26:08 -0500

    Add default sorting

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -1,13 +1,25 @@
 <template>
 	<q-table
+		style="max-height: calc(100vh - 150px)"
 		title="Food"
 		:rows="foods"
+		row-key="name"
 		:columns="columns"
 		no-data-label="Add food to get started!"
 		virtual-scroll
+		:pagination="pagination"
+		:rows-per-page-options="[0]"
 		hide-bottom
 		@row-click="onRowClick"
+		:filter="filter"
 	>
+		<template v-slot:top-right>
+			<q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+				<template v-slot:append>
+					<q-icon name="search" />
+				</template>
+			</q-input>
+		</template>
 		<template #body-cell-safe="props">
 			<q-td :props="props">
 				<q-badge :color="getSafetyColor(props.value)" :label="props.value"/>
@@ -18,6 +30,7 @@
 
 <script setup>
 import {useRouter} from 'vue-router'
+import {ref} from 'vue'
 
 const router = useRouter()
 
@@ -32,11 +45,28 @@ const onRowClick = (row, evt) => {
 	router.push(`/food/${evt.id}`)
 }
 
+const filter=ref('')
+
+const pagination = ref({
+	sortBy: 'date',
+	rowsPerPage: [0]
+})
+
 const columns = [
 	{name: 'name', required: true, label: 'Name', align: 'left', field: 'name'},
 	{name: 'brand', label: 'Brand', align: 'left', field: 'brand', format: val => val ?? 'None'},
 	{name: 'store', label: 'Store', align: 'left', field: 'store', format: val => val ?? 'None'},
 	{name: 'safe', required: true, label: 'Is Safe', align: 'left', field: 'isSafe'},
+	{
+		name: 'date',
+		required: true,
+		label: 'Date Added',
+		align: 'left',
+		field: 'dateAdded',
+		format: val => getDateFormatted({timestamp: val}),
+		sort: (a, b) => b - a,
+		sortable: true
+	},
 ]
 
 const getSafetyColor = (value) => {
@@ -48,5 +78,10 @@ const getSafetyColor = (value) => {
 		default:
 			return 'orange'
 	}
+}
+
+const getDateFormatted = ({timestamp}) => {
+	const date = new Date(timestamp)
+	return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 }
 </script>
